84489ce04ee444d62269313653111d2bb247607c
Caleb Yates <caldog20@gmail.com>
2023-08-16 19:29:56 -0500

    Update Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
+#include <filesystem>
+
 #include "psx.hpp"
 
 auto main() -> int {
     PSX psx;
-    psx.loadBIOS("../SCPH1001.BIN");
-    //        psx.sideload("../tetris.ps-exe");
-    //    psx.loadDisc("/Users/yatesca/Projects/PSXEmulator/bam2.bin");
-    psx.loadDisc("/Users/yatesca/Emulation/yopazicestar/yicestar_ntsc.bin");
+    psx.loadBIOS(std::filesystem::current_path() / "SCPH1001.BIN");
+    psx.sideload("../psxtest_cpu.exe");
     psx.start();
 
     while (psx.isOpen()) {
