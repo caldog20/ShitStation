3283567c88981675625af3ec8c813ee2b8950f89
Caleb Yates <caldog20@gmail.com>
2023-08-21 13:13:14 -0500

    Fix bundling all together

--- .github/bundle.sh
+++ .github/bundle.sh
@@ -2,14 +2,14 @@ PATH="$PATH:/usr/libexec"
 
 mkdir -p ShitStation.app/Contents/MacOS/Libraries
 mv build/ShitStation ShitStation.app/Contents/MacOS
-chmod 777 ShitStation.app/Contents/MacOS/ShitStation
+chmod a+x ShitStation.app/Contents/MacOS/ShitStation
 
-PlistBuddy ${APPROOT}/Contents/Info.plist -c "add CFBundleDisplayName string ShitStation"
-PlistBuddy ${APPROOT}/Contents/Info.plist -c "add CFBundleIconName string AppIcon"
-PlistBuddy ${APPROOT}/Contents/Info.plist -c "add CFBundleIconFile string AppIcon"
-PlistBuddy ${APPROOT}/Contents/Info.plist -c "add NSHighResolutionCapable bool true"
-PlistBuddy ${APPROOT}/Contents/Info.plist -c "add LSMinimumSystemVersion 10.15"
-PlistBuddy ${APPROOT}/Contents/version.plist -c "add ProjectName string ShitStation"
+PlistBuddy ShitStation.app/Contents/Info.plist -c "add CFBundleDisplayName string ShitStation"
+PlistBuddy ShitStation.app/Contents/Info.plist -c "add CFBundleIconName string AppIcon"
+PlistBuddy ShitStation.app/Contents/Info.plist -c "add CFBundleIconFile string AppIcon"
+PlistBuddy ShitStation.app/Contents/Info.plist -c "add NSHighResolutionCapable bool true"
+PlistBuddy ShitStation.app/Contents/Info.plist -c "add LSMinimumSystemVersion 10.15"
+PlistBuddy ShitStation.app/Contents/version.plist -c "add ProjectName string ShitStation"
 
 dylibbundler -od -b -x ShitStation.app/Contents/MacOS/ShitStation -d ShitStation.app/Contents/Frameworks/ -p @rpath
 install_name_tool -add_rpath @loader_path/../Frameworks ShitStation.app/Contents/MacOS/ShitStation
