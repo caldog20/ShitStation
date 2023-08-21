d615493bf20738f1d51d53702bc6ab6e4a03580e
Caleb Yates <caldog20@gmail.com>
2023-08-21 13:33:57 -0500

    fix app bundling

--- .github/bundle.sh
+++ .github/bundle.sh
@@ -1,14 +1,15 @@
 PATH="$PATH:/usr/libexec"
 
 mkdir -p ShitStation.app/Contents/MacOS/Libraries
-mv build/ShitStation ShitStation.app/Contents/MacOS
+mv cmake-build-debug/ShitStation ShitStation.app/Contents/MacOS
 chmod a+x ShitStation.app/Contents/MacOS/ShitStation
 
 PlistBuddy ShitStation.app/Contents/Info.plist -c "add CFBundleDisplayName string ShitStation"
 PlistBuddy ShitStation.app/Contents/Info.plist -c "add CFBundleIconName string AppIcon"
 PlistBuddy ShitStation.app/Contents/Info.plist -c "add CFBundleIconFile string AppIcon"
 PlistBuddy ShitStation.app/Contents/Info.plist -c "add NSHighResolutionCapable bool true"
-PlistBuddy -c "Add :LSMinimumSystemVersion 10.15" ShitStation.app/Contents/Info.plist
+PlistBuddy ShitStation.app/Contents/Info.plist -c "add LSMinimumSystemVersion string 10.15"
+
 PlistBuddy ShitStation.app/Contents/version.plist -c "add ProjectName string ShitStation"
 
 dylibbundler -od -b -x ShitStation.app/Contents/MacOS/ShitStation -d ShitStation.app/Contents/Frameworks/ -p @rpath
